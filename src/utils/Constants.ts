2e0e58cd1e0a62787a32c42c708ecba9601845b9
Gazoh <cem@axastudios.com>
2022-03-14 16:28:06 +0100

    - New Addresses

--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -1,27 +1,29 @@
 // # Pools
-export const SUSHI_GWSDAI_PAIR: string              = '0x1ae194BeBB57a3337a333258d1e93B1079d46a62';
+export const SUSHI_GWSDAI_PAIR: string              = '0x641dBc95Ce7fC9d6F32037Fd288479c4CF39fEd7';
 export const SUSHI_USDC_ETH_PAIR: string            = '0xc20cf81dfF14ce8C9b454e6Cd4B2145d81535849'; // KOVAN TESTNET
 // export const SUSHI_USDC_ETH_PAIR                = '0x397ff1542f962076d0bfe58ea045ffa2d347aca0'; // MAINNET
 
 // # Bonds
 export const GWSDAILPBOND_TOKEN: string             = "GWS-DAI";
-export const GWSDAISLPBOND_CONTRACT: string         = '0xa16b777eEaD3c0C805D489681Cf43DD8cc4523ca';
-export const GWSDAISLPBOND_CONTRACT_BLOCK: string   = '30242758';
+export const GWSDAISLPBOND_CONTRACT: string         = '0x156A5cB34EC70da2896bbfC4BF6C48EfBa4781dE';
+export const GWSDAISLPBOND_CONTRACT_BLOCK: string   = '30391492';
 
 export const DAIBOND_TOKEN: string                  = "DAI";
-export const DAI_ERC20_CONTRACT: string             = '0xEaE9F452134ABd607a2a2e093c6009cc6Aa2ED75'; // !!!: TODO REAL DAI ADDRESS PUT IT UNDER HERE AND COMMENT IT OUT 
-export const DAIBOND_CONTRACT: string               = '0x859C11a5F77F5c8bEbB293AAC1f0d105EcC6C18F';
-export const DAIBOND_CONTRACT_BLOCK: string         = '30242660';
+export const DAI_ERC20_CONTRACT: string             = '0xe320940E7bD8892FC53f958Ed9816f8b2d999910'; // !!!: TODO: TESTNET
+// export const DAI_ERC20_CONTRACT: string             = '0x6B175474E89094C44Da98b954EedeAC495271d0F'; // !!!: TODO: MAINNET
+export const DAIBOND_CONTRACT: string               = '0x54a1eab69eE5b5640611C00a09fB6fC5323F79d1';
+export const DAIBOND_CONTRACT_BLOCK: string         = '30391940';
 
 export const USDCBOND_TOKEN: string                 = "USDC";
-export const USDC_ERC20_CONTRACT: string            = '0xC0db23C1dc9D713C5E5940B8378Ffe71E24240BB'; // !!!: TODO REAL USDC ADDRESS PUT IT UNDER HERE AND COMMENT IT OUT 
-export const USDCBOND_CONTRACT: string              = '0x770Ca093030CF071a158a1868E2107e2Ee9B5102';
-export const USDCBOND_CONTRACT_BLOCK: string        = '30242705';
+export const USDC_ERC20_CONTRACT: string            = '0xE127DE0F8279FDc5Be0F6b136Ce13FfBaf8BE3D9'; // !!!: TODO: TESTNET
+// export const USDC_ERC20_CONTRACT: string            = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'; // !!!: TODO: MAINNET
+export const USDCBOND_CONTRACT: string              = '0x06bf68e7dcc4C10c7bEd151948e3Dab4beCcBC12';
+export const USDCBOND_CONTRACT_BLOCK: string        = '30391421';
 
 // # Addresses
-export const TREASURY_ADDRESS: string               = '0xe8f4992Aa081E3B4B7341251799F3F61B7f0fF69';
-export const GWS_ERC20_CONTRACT: string             = '0x49a97304b3ff52e8471d643884Db196cec867c80';
-export const SGWS_ERC20_CONTRACT: string            = '0xa4B206B5c69b23F4D6DF608Cbf8Ac683063fc50C';
-export const CIRCULATING_SUPPLY_CONTRACT: string    = '0xd5822169D8DaE4439c91eAcCbdE166268dE6eaF6';
-export const STAKING_CONTRACT: string               = '0xaF73f0bd55be48F08a7eE31dd3B76Ba0aa7d11A3';
-export const BONDING_CALCULATOR: string             = '0x30f882B7353b8f5CA4B64e5e443C24Bc4f101B1C';
+export const TREASURY_ADDRESS: string               = '0x070F9bE802CfeB2D042bA45D9e6aCa3cc58ff6aA';
+export const GWS_ERC20_CONTRACT: string             = '0xB505238Ee71F2B4C24BC203937ab344C3cA7ab96';
+export const SGWS_ERC20_CONTRACT: string            = '0x3dbA6F2F4a2b5CD8d3C4f4C2fF4f15a9C6e9e834';
+export const CIRCULATING_SUPPLY_CONTRACT: string    = '0xd5783D9bFe1d8E736af9b979657C8D6DEA0bDc86';
+export const STAKING_CONTRACT: string               = '0x376A1b1E07f9358791e8c794cD4b666A535F96cC';
+export const BONDING_CALCULATOR: string             = '0x2bd54DD48B98C3F37a1627cC4d8093266234e814';
